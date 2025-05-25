73f18f37b8f1664c102ce1a4a6964a42ca8ac1eb
GorgonUK <georgekontus@gmail.com>
2025-05-25 01:17:16 +0100

    PDF fix

--- Dropzone/utils.ts
+++ Dropzone/utils.ts
@@ -148,3 +148,24 @@ function getFocusedTab() {
   context: ComponentFramework.Context<any>,
   key: LocalizationKey
 ): string => context.resources.getString(key);
+
+export function b64toBlob(b64Data: string, contentType = '', sliceSize = 512) {
+  if (b64Data.startsWith("data:application/pdf;base64,")) {
+    b64Data = b64Data.replace("data:application/pdf;base64,", "");
+  }
+
+  const byteCharacters = atob(b64Data);
+  const byteArrays = [];
+
+  for (let offset = 0; offset < byteCharacters.length; offset += sliceSize) {
+    const slice = byteCharacters.slice(offset, offset + sliceSize);
+    const byteNumbers = new Array(slice.length);
+    for (let i = 0; i < slice.length; i++) {
+      byteNumbers[i] = slice.charCodeAt(i);
+    }
+    byteArrays.push(new Uint8Array(byteNumbers));
+  }
+
+  return new Blob(byteArrays, { type: contentType });
+}
+
